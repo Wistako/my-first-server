c70f4cf37d4523eb2547469aec451bf60228574b
Piotr Słomczyński <wistako@gmail.com>
2024-03-04 14:45:21 +0100

    Use handlebars

--- server.js
+++ server.js
@@ -1,31 +1,50 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
+
 
 const app = express();
+app.engine('.hbs', hbs({ extname: '.hbs', layoutsDir: './layouts', defaultLayout: 'main'}));
+app.set('view engine', '.hbs');
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
+// mildware
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/user', (req, res, next) => {
-  res.show('login.html');
+  res.render('login');
 });
+
+
+// endpoints
 
 app.get('/', (req, res) => {
-  res.show('index.html');
+  // res.render('index', { layout: false });
+  res.render('index', { layout: 'dark' });
 });
 
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { name: req.params.name });
+})
+
 app.get('/about', (req, res) => {
-  res.show('about.html');  
+  res.render('about');  
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.get('/history', (req, res) => {
+  res.render('history');
+});
+
+app.get('/info', (req, res) => {
+  res.render('info');
 });
 
 app.use((req, res) => {
-  res.status(404).show('404.html');
+  res.status(404).render('404');
 });
 
 app.listen(8000, () => {
